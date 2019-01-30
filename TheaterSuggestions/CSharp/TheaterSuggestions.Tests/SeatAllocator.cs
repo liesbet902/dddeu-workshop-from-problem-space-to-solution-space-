eb3c51402368cb9ed889ffa1d96e2e00917178d5
liesbetjanssens <liesbet.janssens@protime.eu>
2019-01-30 11:31:08 +0100

    Get no seats when all reserved Get first seats when all available

---
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatAllocator.cs
@@ -0,0 +1,53 @@
+﻿using ExternalDependencies.AuditoriumLayoutRepository;
+using ExternalDependencies.ReservationsProvider;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeatsSuggestions.Tests
+{
+    public class SeatAllocator
+    {
+        public SeatAllocator(ReservationsProvider reservations, AuditoriumLayoutRepository auditoriumLayout)
+        {
+            Reservations = reservations;
+            AuditoriumLayout = auditoriumLayout;
+        }
+
+        public ReservationsProvider Reservations { get; }
+
+        public AuditoriumLayoutRepository AuditoriumLayout { get; }
+   
+        internal SuggestionMade MakeSuggestions(string showId, int party)
+        {
+            var layout = AuditoriumLayout.GetAuditoriumLayoutFor(showId);
+            var seat = layout.Rows["A"][0];
+
+            var reservations = Reservations.GetReservedSeats(showId);
+
+            var suggestedSeats = new List<Seat>();
+            if (!reservations.ReservedSeats.Contains(seat.Name))
+                suggestedSeats.Add(new Seat(seat.Name));
+
+            return new SuggestionMade()
+            {
+                Seats = suggestedSeats
+            };
+        }
+    }
+
+    internal class SuggestionMade
+    {
+        public List<Seat> Seats { get; set; }
+    }
+
+    public struct Seat
+    {
+        public string SeatLocation { get; set; }
+
+        public Seat(string seatLocation)
+        {
+            SeatLocation = seatLocation;
+        }
+    }
+}
--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatsAllocatorShould.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatsAllocatorShould.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using ExternalDependencies.ReservationsProvider;
+using ExternalDependencies.AuditoriumLayoutRepository;
+using NUnit.Framework;
+using NFluent;
+using System.Linq;
 
 namespace SeatsSuggestions.Tests
 {
@@ -6,5 +10,32 @@ namespace SeatsSuggestions.Tests
     public class SeatsAllocatorShould
     {
         // TODO: add your first acceptance test here
+        [Test]
+        public void SeatsNotAvailable_WhenAlreadyReserved()
+        {
+            var showId_AllSeatsReserved = "5";
+            var party = 1;
+
+            var reservations = new ReservationsProvider();
+            var auditoriumLayout = new AuditoriumLayoutRepository();
+            var seatAllocator = new SeatAllocator(reservations, auditoriumLayout);
+            SuggestionMade suggestionMade = seatAllocator.MakeSuggestions(showId_AllSeatsReserved, party);
+
+            Check.That(suggestionMade.Seats).IsEmpty();
+        }
+
+        [Test]
+        public void SeatsAvailable_PartyOfOne_ReturnFirstSeat()
+        {
+            var showId_AllSeatsAvailable = "8";
+            var party = 1;
+            var reservations = new ReservationsProvider();
+            var auditoriumLayout = new AuditoriumLayoutRepository();
+
+            var seatAllocator = new SeatAllocator(reservations, auditoriumLayout);
+            SuggestionMade suggestionMade = seatAllocator.MakeSuggestions(showId_AllSeatsAvailable, party);
+
+            Check.That(suggestionMade.Seats).Contains(new Seat("A1"));
+        }
     }
 }
